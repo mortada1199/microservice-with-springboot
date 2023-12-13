2af0c263f2992c624dbef336ec1a8f3e45cbc430
murtada abd algalil <murtada199815@gmail.com>
2023-12-13 12:53:48 +0200

    add eurka-server

--- customer/src/main/java/com/microservice/service/CustomerService.java
+++ customer/src/main/java/com/microservice/service/CustomerService.java
@@ -2,14 +2,17 @@ package com.microservice.service;
 
 import com.microservice.entity.Customer;
 import com.microservice.entity.CustomerRegistrationRequest;
+import com.microservice.entity.FraudCheckResponse;
 import com.microservice.repo.CustomerRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
 @AllArgsConstructor
-public class CustomerService{
+public class CustomerService {
     private final CustomerRepository customerRepository;
+    private final RestTemplate restTemplate;
 
 //    public CustomerService(CustomerRepository customerRepository) {
 //        this.customerRepository = customerRepository;
@@ -19,8 +22,18 @@ public class CustomerService{
         Customer customer = Customer.builder().
                 firstName(register.firstName()).
                 lastName(register.lastName()).
-                email(register.email()).build();
+                email(register.email())
+                .build();
 
-        customerRepository.save(customer);
+        customerRepository.saveAndFlush(customer);
+        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
+                "http://localhost:8081/api/v1/fraud-check/{customerId}",
+                FraudCheckResponse.class,
+                customer.getId()
+        );
+        if (fraudCheckResponse.isFraudster()){
+            throw new IllegalStateException("Fraudster");
+        }
+
     }
 }
--- fraud/src/main/java/com/microservice/controller/FraudCheckController.java
+++ fraud/src/main/java/com/microservice/controller/FraudCheckController.java
@@ -3,11 +3,13 @@ package com.microservice.controller;
 import com.microservice.entity.FraudCheckResponse;
 import com.microservice.service.FraudCheckService;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/v1/fraud-check")
 @AllArgsConstructor
+@Slf4j
 public class FraudCheckController {
    private final FraudCheckService fraudCheckService;
 
@@ -19,6 +21,7 @@ public class FraudCheckController {
     public FraudCheckResponse isFraudster(@PathVariable("customerId") Integer customerId){
 
         boolean  result = fraudCheckService.isFraudulentCustomer(customerId);
+        log.info("fraud check yhe customer of id {}",customerId);
         return  new FraudCheckResponse(result);
 }
 
